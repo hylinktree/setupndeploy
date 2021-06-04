bbe6c6a41499bc328aa3d09f5fa434c82daa35d4
hylinktree@home <hylinktree@home.com>
2021-06-04 22:13:40 +0800

    nocomment

--- deploy/basenginx/run.sh
+++ deploy/basenginx/run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-PORTMAP=80:80
+PORTMAP="-p 80:80 -p 443:443"
 IMGNAME=gdca/basenginx
 CNTNAME=c_nginx
 if [ x$1 = x ]; then
 	docker stop ${CNTNAME} 2>/dev/null
 	docker rm ${CNTNAME} 2>/dev/null
-	docker run --name ${CNTNAME} --restart always -d -p $PORTMAP $IMGNAME
+	docker run --name ${CNTNAME} --restart always -d $PORTMAP $IMGNAME
 else
-	docker run --rm -d -p $PORTMAP $IMGNAME
+	docker run --rm -it $PORTMAP $IMGNAME bash
 fi
