067eed84ce8b48296369ff249ec03eedcf55c776
朱玄義 <hsiuenyichu@insnergy.com>
2021-06-09 12:44:15 +0800

    nocomment

--- deploy/docker-registry/mkuser.sh
+++ deploy/docker-registry/mkuser.sh
@@ -1,13 +1,15 @@
+rm -rf auth 2>/dev/null 
 mkdir auth 2>/dev/null
 htpasswd -Bc auth/registry.password iii 
-docker run --rm -it \
-  -p 5009:5000 \
-  -v "$(pwd)"/auth:/auth \
-  -e "REGISTRY_AUTH=htpasswd" \
-  -e "REGISTRY_AUTH_HTPASSWD_REALM=Registry Realm" \
-  -e REGISTRY_AUTH_HTPASSWD_PATH=/auth/htpasswd \
-  -v "$(pwd)"/certs:/certs \
-  -e REGISTRY_HTTP_TLS_CERTIFICATE=/certs/auth.crt \
-  -e REGISTRY_HTTP_TLS_KEY=/certs/auth-no.key \
-  -v "$(pwd)"/data:/data \
-  registry:2 
+
+# docker run --rm -it \
+#   -p 5009:5000 \
+#   -e "REGISTRY_AUTH=htpasswd" \
+#   -e "REGISTRY_AUTH_HTPASSWD_REALM=Registry Realm" \
+#   -e REGISTRY_AUTH_HTPASSWD_PATH=/auth/htpasswd \
+#   -e REGISTRY_HTTP_TLS_CERTIFICATE=/certs/auth.crt \
+#   -e REGISTRY_HTTP_TLS_KEY=/certs/auth-no.key \
+#   -v "$(pwd)"/data:/data \
+#   -v "$(pwd)"/auth:/auth \
+#   -v "$(pwd)"/certs:/certs \
+#   registry:2 
