094660cd847b74ac976276aed8af815c16fca6c9
hychu@hpcargo <hychu@hpcargo>
2021-06-25 16:31:45 +0800

    nocomment

--- deploy/install-gradle.sh
+++ deploy/install-gradle.sh
@@ -1,2 +1,8 @@
+# install OpenJDK11U-jdk_x64_linux_openj9_11.0.11_9_openj9-0.26.0.tar.gz 
 VERSION=6.5.1
 wget https://services.gradle.org/distributions/gradle-${VERSION}-bin.zip -P /tmp
+unzip -d /opt/gradle /tmp/gradle-${VERSION}-bin.zip
+ln -s /opt/gradle/gradle-${VERSION} /opt/gradle/latest
+# /etc/profile.d/gradle.sh
+# export GRADLE_HOME=/opt/gradle/latest
+# export PATH=${GRADLE_HOME}/bin:${PATH}
