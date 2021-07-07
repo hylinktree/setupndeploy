9fbfcc3f9142c870c055e546f6aa7f5b31903678
hychu@hpcargo <hychu@hpcargo>
2021-07-07 20:58:23 +0800

    nocomment

--- deploy/nats-js/run-nsc.sh
+++ deploy/nats-js/run-nsc.sh
@@ -1,1 +1,1 @@
-docker run --rm -it --network host -v /root/github/setupndeploy/deploy/nats-js/nsc.confd:/nsc synadia/nats-box:latest
+docker run --rm -it --network host -v /root/github/setupndeploy/deploy/nats-js/certs.d:/certs.d -v /root/github/setupndeploy/deploy/nats-js/nsc.confd:/nsc synadia/nats-box:latest
